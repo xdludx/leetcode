f001fd045ee036c9f81d8c4c7bfbda8400818a7f
卢东肖 <ludongxiao@meituan.com>
2020-02-22 16:33:44 +0800

    括号生成二叉树算法

--- 22. 括号生成.js
+++ 22. 括号生成.js
@@ -1,21 +1,54 @@
 /**
  * https://leetcode-cn.com/problems/generate-parentheses/
- * TODO：可以使用深度优先遍历算法，待优化。
  * @param {number} n
  * @return {string[]}
  */
+
+
+// var generateParenthesis = function(n) {
+//     if (n === 0) return [];
+//     if (n === 1) return ['()'];
+//     const preParenthesis = generateParenthesis(n - 1);
+//     const ret = new Set();
+//     for (let i = 0; i < preParenthesis.length; i++) {
+//         const str = preParenthesis[i];
+//         for (let j = 0; j < str.length; j++) {
+//             ret.add(str.slice(0, j) + '()' + str.slice(j));
+//         }
+//     }
+//     return Array.from(ret);
+// };
+
+/**
+ * 二叉树遍历
+ *
+ * @param {*} n
+ */
 var generateParenthesis = function(n) {
-    if (n === 0) return [];
-    if (n === 1) return ['()'];
-    const preParenthesis = generateParenthesis(n - 1);
-    const ret = new Set();
-    for (let i = 0; i < preParenthesis.length; i++) {
-        const str = preParenthesis[i];
-        for (let j = 0; j < str.length; j++) {
-            ret.add(str.slice(0, j) + '()' + str.slice(j));
+    const result = [];
+    dfs(n, n, '', result);
+    return result;
+}
+/**
+ *
+ * @param {*} left 左括号数量
+ * @param {*} right 右括号数量
+ * @param {*} str 
+ * @param {*} result 结果
+ */
+function dfs(left, right, str, result) {
+    if (left > right) return; // 剩下的左括号数量大于右括号数量直接返回，则不符合条件，直接返回
+    if (left === 0 && right === 0) {
+        result.push(str);
+    } else {
+        console.log(left, right, str, result)
+        if (left > 0) {
+            dfs(left - 1, right, str + '(', result);
+        }
+        if (right > 0) {
+            dfs(left, right - 1, str + ')', result);
         }
     }
-    return Array.from(ret);
-};
+}
 
 console.log(generateParenthesis(3));
