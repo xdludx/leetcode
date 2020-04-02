b4f1caee5022421a86d4ad70cf93109f729ac3f9
卢东肖 <ludongxiao@meituan.com>
2020-04-03 01:18:37 +0800

    二分法

--- 35. 搜索插入位置.js
+++ 35. 搜索插入位置.js
@@ -4,14 +4,34 @@
  * @param {number} target
  * @return {number}
  */
+
+// const searchInsert = function(nums, target) {
+//     let index = 0;
+//     while (index < nums.length) {
+//         const current = nums[index];
+//         if (target <= current) break;
+//         index++
+//     }
+//     return index;
+// };
+
 const searchInsert = function(nums, target) {
-    let index = 0;
-    while (index < nums.length) {
-        const current = nums[index];
-        if (target <= current) break;
-        index++
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        }
+        if (nums[mid] < target) {
+            left = mid + 1;
+        }
+        if (nums[mid] > target) {
+            right = mid - 1;
+        }
     }
-    return index;
+    return left;
 };
 
 console.log(searchInsert([1,3,5,6], 2));
